83667fb2d458c060bed932f25e30b9ac3b3e7c50
amaiurmilla <millaamaiur@gmail.com>
2025-06-18 17:35:55 +0200

    Remove recommended products feature

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,6 @@ self.addEventListener('install', e => {
           './',
           './index.html',
           './style.css',
-          './products.html',
           './script.js',
           './calculator.js',
           './manifest.json',
